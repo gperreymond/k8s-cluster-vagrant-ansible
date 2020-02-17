9b4d9f55607ad0846a5b74cf147aca3f3ebbf9cd
Gilles Perreymond <gperreymond@gmail.com>
2020-02-17 16:32:47 +0100

    feat(): start implements volumes

--- setup.sh
+++ setup.sh
@@ -28,5 +28,8 @@ helm upgrade metallb --namespace metallb-system --install kubernetes/metallb --v
 # Helm: Install "Nginx Ingress"
 helm upgrade nginx-ingress --install kubernetes/nginx-ingress --values kubernetes/nginx-ingress-values.yaml
 
+# Helm: Install "Rabbitmq"
+helm upgrade rabbitmq --namespace rabbitmq-system --install kubernetes/rabbitmq --values kubernetes/rabbitmq-values.yaml
+
 # Helm: Install "Kubernetes Dashboard"
 helm upgrade dashboard --namespace kube-system --install kubernetes/kubernetes-dashboard --values kubernetes/kubernetes-dashboard-values.yaml
